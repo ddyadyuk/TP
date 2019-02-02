76ca3e215f746f8d1f139035d053452e96c39458
dd <ddyadyuk507@gmail.com>
2019-02-02 20:24:57 +0300

    js was improved

--- src/Lab3/script.js
+++ src/Lab3/script.js
@@ -1,6 +1,33 @@
 $(document).ready(function () {
-    $(".firstButton").click()
-        .ajax(function () {
+    $(".firstButton").click(function () {
+        $(".gameWindow").addClass("grid");
+        });
 
-        })
+        $(".create").click(function(){
+
+            var values = {};
+            $.each($("form").serializeArray(), function (i, field) {
+                values[field.name] = field.value;
+
+
+            });
+
+            var getValue = function (valueName) {
+                return values[valueName];
+            }
+            var radio = getValue("radio");
+            var text = getValue("Text");
+            var color = getValue("Color");
+
+            if(radio === $("form.p:first-child")){
+                $(".workWindow").prepend('<p '+'style="background-color:'+color+'; " '+'>' + text +'</p>');
+            } else {
+                $(".workWindow").append('<p '+'style="background-color:'+color+'; " '+'>' + text +'</p>');
+            }
+        });
+
+
+    // $("button").click(function () {
+    //     $(".workWindow").append('<p>'+$("#formFirst").serialize() + +'</p>' + '<br/>');
+     //})
 });
