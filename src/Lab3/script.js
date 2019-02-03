75d640ce256c61eeab5f989e8a7970b24d0b479c
dd <ddyadyuk507@gmail.com>
2019-02-03 15:34:07 +0300

    second task was solved

--- src/Lab3/script.js
+++ src/Lab3/script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     $(".firstButton").click(function () {
         $(".gameWindow").addClass("grid");
         });
-
         $(".create").click(function(){
 
             var values = {};
@@ -27,5 +26,54 @@ $(document).ready(function () {
 
         });
 
+    $(".secondButton").click(function () {
+        $(".secondGameWindow").addClass("grid");
+    });
+    $(".createRectangle").click(function () {
+        var colorsArray = ["red", "white", "blue", "orange", "black", "darkgrey", "darkcyan", "gainsboro", "blueviolet", "mediumspringgreen", "firebrick", "purple", "hotpink", "aqua", "royalblue"];
+        var currentColor = 0;
+        while (currentColor === 0) {
+            var i = Math.floor((Math.random() * colorsArray.length))
+            currentColor = colorsArray[i];
+        }
+        var divWidth = $(".secondWorkWindow").width();
+        var divHeight = $(".secondWorkWindow").height();
+        var absolute = "relative";
+        var randPosX = Math.floor(Math.random() * divWidth);
+        var randPosY = Math.floor((Math.random()) * divHeight);
+        for (var i = 1; i <= 1; i++) {
+            if ($("div").is(".createdDiv")) {
+                $(".createdDiv").remove();
+            }
+            $(".secondWorkWindow").append('<div class="createdDiv" ></div>');
+            $(".createdDiv").css('left', randPosX);
+            $(".createdDiv").css("top", randPosY);
+            $(".createdDiv").css("background-color", currentColor);
+            $(".createdDiv").css("height", Math.floor(Math.random() * 150));
+            $(".createdDiv").css("width", Math.floor(Math.random() * 300));
+            $(".createdDiv").css("position", absolute);
 
+
+            window.onkeydown = function () {
+                if (event.keyCode == 37) {
+                    randPosX = randPosX - 10;
+                    console.log(randPosX);
+                    $(".createdDiv").css('left', randPosX);
+                } else if (event.keyCode == 39) {
+                    randPosX = randPosX + 10;
+                    $(".createdDiv").css('left', randPosX);
+                }
+
+                if (event.keyCode == 38) {
+                    randPosY = randPosY - 10;
+                    $(".createdDiv").css("top", randPosY);
+                } else if (event.keyCode == 40) {
+                    randPosY = randPosY + 10;
+                    $(".createdDiv").css("top", randPosY);
+                }
+
+
+            }
+        }
+    });
 });
